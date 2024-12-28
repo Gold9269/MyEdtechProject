ca5a51a7808b05e4bdb50819a1a0896cf0fc255d
Nishant Chopra <nishantchopra2020@gmail.com>
2024-12-28 11:50:49 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://my-mega-project-01-frontend.vercel.app/",
 		credentials: true,
 	})
 );
